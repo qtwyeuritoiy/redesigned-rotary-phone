8dc0b1fd9fc4b6cb8e2a23176df59a34cdc5d338
Jihoon Kim <imsesaok@tuta.io>
2020-09-01 01:23:48 +0800

    Remove hardcoded SECRET_KEY

--- redesigned_rotary_phone/settings.py
+++ redesigned_rotary_phone/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o9ogqgq2$d+0_**2w(kju@ye^v0ggan@_b5i+5m15$j(yt1f$='
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
