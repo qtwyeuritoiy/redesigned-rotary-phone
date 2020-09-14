60d50ce66faaa45fa127f39930d1ee5aedf30939
Michael <michaelxsr@gmail.com>
2020-09-14 12:30:10 +0800

    Added optional parameter to scrape from offline (saved) webpage.

--- crawler/benchmark_crawler.py
+++ crawler/benchmark_crawler.py
@@ -7,20 +7,26 @@ from urllib.request import urlopen
 CPU_URL = 'https://www.cpubenchmark.net/cpu_list.php'
 GPU_URL = 'https://www.videocardbenchmark.net/gpu_list.php'
 
-def scrape(website:str, filename:str) -> str:
+def scrape(website:str, filename:str, selection:str='web') -> str:
     # a function that scrapes a website, saves as CSV format, and returns the path to file
-    with urlopen(website) as website:
-        soup = BeautifulSoup(website, 'html.parser')
-        table = soup.find('table', attrs={'id' : 'cputable', 'class': 'cpulist'})
-        headers = [header.text for header in table.find('thead').find_all('th')]
-        rows = []
+    if selection == 'web':
+        with urlopen(website) as website:
+            soup = BeautifulSoup(website, 'html.parser')
+    elif selection == 'file':
+        file = open(website, 'r')
+        soup = BeautifulSoup(file, 'html.parser')
+        file.close()
 
-        for row in table.find_all('tr'):
-            rows.append([val.text.replace(',', '').replace('NA', '') for val in row.find_all('td')])
+    table = soup.find('table', attrs={'id' : 'cputable', 'class': 'cpulist'})
+    headers = [header.text for header in table.find('thead').find_all('th')]
+    rows = []
 
-        with open(filename, 'w', encoding='utf-8', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(headers)
-            writer.writerows(row for row in rows if row)
+    for row in table.find_all('tr'):
+        rows.append([val.text.replace(',', '').replace('NA', '') for val in row.find_all('td')])
 
-            return os.path.join(os.getcwd(), f.name)
+    with open(filename, 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(row for row in rows if row)
+
+    return os.path.join(os.getcwd(), f.name)
